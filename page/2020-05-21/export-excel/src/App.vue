a7d2a41aa07e822e24106ab55f7f0bdffb99d7dd
aloneWang <alone0228@126.com>
2020-05-27 00:52:45 +0800

    数据自定义导出excel

--- page/2020-05-21/export-excel/src/App.vue
+++ page/2020-05-21/export-excel/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <Table :columns="columns1" :data="data1" ref="table"></Table>
     <Button @click="downLoad('t2t')">下载（table to book）</Button>
+    <Button @click="downLoad('d2b')">下载（数据自定义下载）</Button>
   </div>
 </template>
 <script>
@@ -22,6 +23,10 @@ export default {
         {
           title: "Address",
           key: "address"
+        },
+        {
+          title: 'date',
+          key: 'date'
         }
       ],
       data1: [
@@ -61,7 +66,7 @@ export default {
       switch(type) {
         case 't2t':
           // 表格直转 book
-          let wb = XLSX.utils.table_to_book(el)
+          let wb = XLSX.utils.table_to_book(el,{ raw: true})
           console.log(`wb\n${wb}`)
           // 生成指定文件
           let wbout = XLSX.write(wb,{
@@ -70,13 +75,52 @@ export default {
             bookType: 'xlsx'
           })
           console.log(`wb\n${wbout}`)
+          // XLSX.writeFile(wb, '文件.xlsx', {
+          //   bookSST: false,
+          //   type: 'array',
+          //   bookType: 'xlsx'
+          // })
           let blob = new Blob([wbout], {type: 'application/octet-stream'}) // blob 文件对象
           console.log(`wb\n${blob}`)
           fileSaver.saveAs(blob, '文件.xlsx')
-
+          break
+        case 'd2b':
+          // 数据 -> sheet -> book -> excel
+          // aoa_to_sheet
+          const keys = this.columns1.map( it => it.key)
+          const thead = this.columns1.map( it => it.title)
+          const data = this.data1.map( it => {
+              let _data = []
+              for(let i in it) {
+                if(keys.includes(i)) {
+                  _data.push(it[i])
+                }
+              }
+              return _data
+          })
+          console.log([thead, ...data])
+          const sheet = XLSX.utils.aoa_to_sheet([thead, ...data])
+          console.log(sheet)
+          this.sheetToExcel(sheet)
+          break
+        default:;
       }
     },
-
+    // sheet -> book
+    sheetToExcel(sheet, sheetName = "sheet1") {
+      let workbook = {
+        SheetNames: [sheetName],
+        Sheets: {
+          [sheetName]: sheet
+        }
+      }
+      let wpts = {
+        type: 'binary',
+        bookType: 'xlsx',
+        bookSST: false,
+      }
+      XLSX.writeFile(workbook,'文件.xlsx', wpts)
+    }
   }
 };
 </script>
