e0ca856bc88b44030966a5b866dd493a5c5830cd
aloneWang <alone0228@126.com>
2020-06-14 00:59:48 +0800

    update

--- handWrite/new.js
+++ handWrite/new.js
@@ -5,9 +5,40 @@
  */
 // new fn()
 // var fn = function(a) {this.a = a}
- var newFn = function(fn, argeuments) {
+ var newFn = function() {
   var obj = {}
-  fn.apply(obj, [].slice.call(argeuments,1))
+  let fn = [].shift.call(arguments)
+  obj.__proto__ = fn.prototype
+
+  fn.apply(obj, arguments)
   return obj
  }
  // newFn(fn,argumens)
+
+ // 模拟实现 call
+ Function.prototype.newCall = function(context) {
+  if(typeof context !== 'object') throw new Error('缺少this对象')
+  var obj = context || window
+  obj.fn = this
+  var result = obj.fn(...[...arguments].slice(1))
+  delete obj.fn
+  return result
+ }
+// apply 
+// fn.apply(obj,[])
+Function.prototype.newApply = function(context) {
+  var arr = [...arguments].slice(1)
+  return Function.prototype.newCall(context,...arr)
+}
+
+// bind(this.params)
+
+Function.prototype.newBind = function(context) {
+  var fn = this
+  var params = [...arguments].slice(1)
+  var obj = context || window
+  obj.fn = fn
+  return function() {
+    obj.fn(...params,...arguments)
+  }
+}
